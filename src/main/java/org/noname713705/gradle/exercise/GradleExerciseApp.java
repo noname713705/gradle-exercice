8dda005e3bf1ad64f690951a4dd7ad590f687a9b
jhe <le.jhe@free.fr>
2021-12-07 23:39:14 +0100

    testing utf8

--- src/main/java/org/noname713705/gradle/exercise/GradleExerciseApp.java
+++ src/main/java/org/noname713705/gradle/exercise/GradleExerciseApp.java
@@ -2,6 +2,6 @@ package org.noname713705.gradle.exercise;
 
 public class GradleExerciseApp {
 	public static void main(String[] args) {
-		System.out.println("Hello, world !");
+		System.out.println("Hello, world ! Hé Hé");
 	}
 }
